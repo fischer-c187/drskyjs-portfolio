ebf509f4dca51bdeabe59a540684b57856b89ed3
drsky <contact@drsky.ovh>
2024-03-11 10:15:33 +0100

    feat: Add navigation and social networks configuration, and siteConfig for general configuration

---
+++ src/config/socialNetworks.ts
@@ -0,0 +1,16 @@
+const socialNetworksItems = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/drskyjs',
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/morgandealmeida/',
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com/fischer-c187',
+  },
+];
+
+export default socialNetworksItems;
