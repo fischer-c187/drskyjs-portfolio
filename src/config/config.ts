bdabd8d9b6c751af9e0beee7355fd920945ac66d
drsky <contact@drsky.ovh>
2024-03-13 13:51:19 +0100

    feat: implement skills section

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ type ConfigType = {
   name: string;
   heroText: string[];
   heroImage: ImageMetadata;
+  skills: {
+    [key: string]: string[];
+  };
 };
 
 const siteConfig: ConfigType = {
@@ -13,9 +16,13 @@ const siteConfig: ConfigType = {
   name: 'De Almeida Morgan',
   heroText: [
     "Durant ma formation, au contact de mon mentor, j'ai développé de bonnes pratiques essentielles pour écrire du code réutilisable, maintenable et lisible. Cet apprentissage intensif, guidé par l'expertise et les retours d'un professionnel aguerri, a été un véritable tremplin. Il m'a permis non seulement de comprendre la valeur d'un code bien structuré mais aussi de m'engager pleinement dans la création d'applications web accessibles et inclusives.",
-    "L'apprentissage continu est au cœur de ma pratique professionnelle. Le développement web évolue rapidement, et rester à jour avec les dernières technologies et méthodologies est crucial pour moi. Mon portfolio présente une sélection de projets qui illustrent mon approche du développement front-end, ma capacité à relever des défis techniques et mon engagement envers l'accessibilité et l'innovation.",
   ],
   heroImage: profilPicture,
+  skills: {
+    Frontend: ['Typescript', 'Javascript', 'ReactJS', 'HMLT/CSS', 'Tailwind css'],
+    Backend: ['NodeJS', 'Express', 'MongoDB', 'MySQL'],
+    Tools: ['Docker', 'Git', 'Github actions', 'Vitest', 'Playwright', 'Figma'],
+  },
 };
 
 export default siteConfig;
